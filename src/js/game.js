6b35bb48463d9e7a367b4090252553714ef32203
Anton <anton.solovev.1990@mail.ru>
2023-04-13 14:47:35 +0700

    2 задание

---
+++ src/js/game.js
@@ -0,0 +1,26 @@
+import Character from "/domain";
+
+export default class Game {}
+
+export class GameSavingData {}
+
+export function readGameSaving() {}
+
+export function writeGameSaving() {}
+
+
+
+class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  class GameSavingData {
+  }
+  
+  function readGameSaving() {
+  }
+  
+  function writeGameSaving() {
+  }
